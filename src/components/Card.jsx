ebf3026a103a7e20b705066e4fc5adca4a263091
Administrador <administrador@ubuntudocker.myguest.virtualbox.org>
2024-04-30 13:15:39 -0400

    Curso React JS

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return(
+        <div className="card">
+            <img src="" alt="" />
+            <h2>Bro Code</h2>
+            <p>I make Youtube videos and play video games</p>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; Your Website</p>
+        </footer>
+    );
+}
+
+export default Footer;
